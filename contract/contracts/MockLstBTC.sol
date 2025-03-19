fcc0a7b407a4a1b6a54113602e12fd2587740378
Ashwin-3cS <flashashwin2004@gmail.com>
2025-03-20 00:55:35 +0530

    lstBTC.sol added

---
+++ contract/contracts/MockLstBTC.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract LstBTC is ERC20, Ownable {
+    constructor() ERC20("Liquid Staked BTC", "lstBTC") Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
